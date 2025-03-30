49a70280998c3ffa9d5c060a7493658de457d67b
ryousuke.mizokami <ming10r1170@gmail.com>
2025-03-30 11:39:53 +0900

    環境によってHTTPSの強制を切り替える

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('https'); //リンクをHTTPSにする設定を行う
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
